5a5fd1e5c25eb67738abee38d3c2cd62bddf9278
Josefernandezd7 <142596446+Josefernandezd7@users.noreply.github.com>
2023-10-20 08:41:35 -0400

    Add files via upload

---
+++ Q2WritingFile/WriteToFileExample.java
@@ -0,0 +1,20 @@
+package Q2WritingFile;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class WriteToFileExample {
+    public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new FileReader("sample.txt")))
+        {
+            String line;
+            while((line = br.readLine())!= null){
+            System.out.println(line);
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
